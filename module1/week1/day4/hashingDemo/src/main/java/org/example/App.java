d184ac92fb39a1495a5cb4b90bd05d94475b4aad
JWardle245 <74054752+JWardle245@users.noreply.github.com>
2021-05-14 14:18:57 +0100

    Day 5 work - encryption added to hashing demo project

--- module1/week1/day4/hashingDemo/src/main/java/org/example/App.java
+++ module1/week1/day4/hashingDemo/src/main/java/org/example/App.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import org.jasypt.util.text.AES256TextEncryptor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class App 
 {
     public static void main( String[] args )
     {
+        // HASHING DEMO
+        /*
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String hashedPassword = passwordEncoder.encode("your password");
 
@@ -14,5 +17,19 @@ public class App
 
         boolean isMatch = passwordEncoder.matches("your password", "$2a$10$mUkmYq9C4wlPLwzqqE1NlO.jpNlyIkoGL6Amn9ZuN2Z.8Yf8OnBia");
         System.out.println("Passwords match: " + isMatch);
+        */
+
+        //ENCRYPTION DEMO
+
+        AES256TextEncryptor textEncryptor = new AES256TextEncryptor();
+        String myText = "Encrypt this";
+        textEncryptor.setPassword("PASSWORD");
+        System.out.println("Original password: " + myText);
+
+        String myEncryptedText = textEncryptor.encrypt(myText);
+        System.out.println("Encrypted password: " + myEncryptedText);
+
+        String plainText = textEncryptor.decrypt(myEncryptedText);
+        System.out.println("Decrypted password: " + plainText);
     }
 }
